fb3468f0e9b531352ac9f1b97817b52a0b579ce4
rsiddharth <s@ricketyspace.net>
2020-08-29 10:46:54 -0400

    c1.go -> challenge/c1.go

--- challenge/c1.go
+++ challenge/c1.go
@@ -1,14 +1,14 @@
 // Copyright © 2020 rsiddharth <s@ricketyspace.net>
 // SPDX-License-Identifier: ISC
 
-package main
+package challenge
 
 import (
 	"fmt"
 	"ricketyspace.net/cryptopals/enc"
 )
 
-func main() {
+func C1() {
 	hex := "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
 	b64 := enc.HexToBase64(hex)
 
