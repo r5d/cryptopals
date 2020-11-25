f17751d48f480bbaba999ca4357096128e125c6e
rsiddharth <s@ricketyspace.net>
2020-11-25 00:10:52 -0500

    lib: blocks: add BlocksEqual

--- lib/blocks.go
+++ lib/blocks.go
@@ -47,3 +47,16 @@ func TransposeBlocks(blocks [][]byte, keysize int) [][]byte {
 	}
 	return tblocks
 }
+
+func BlocksEqual(a, b []byte) bool {
+	if len(a) != len(b) {
+		return false
+	}
+
+	for i := 0; i < len(a); i++ {
+		if a[i] != b[i] {
+			return false
+		}
+	}
+	return true
+}
