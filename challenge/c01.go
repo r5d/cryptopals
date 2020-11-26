2e840344d0d4ad80328c595ad0fc1e7afa971777
rsiddharth <s@ricketyspace.net>
2020-11-25 21:21:15 -0500

    challenge: c01: add output

--- challenge/c01.go
+++ challenge/c01.go
@@ -14,3 +14,7 @@ func C1() {
 
 	fmt.Printf("b64(0x%v) = %v\n", hex, b64)
 }
+
+// Output:
+//
+// b64(0x49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d) = SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t
