c301e9ee2dfbfb8e10b3fe26480fb15091d26e2d
rsiddharth <s@ricketyspace.net>
2020-11-28 22:11:57 -0500

    challenge/09: change block size to 20

--- challenge/c09.go
+++ challenge/c09.go
@@ -10,12 +10,11 @@ import (
 
 func C9() {
 	in := lib.StrToBytes("YELLOW SUBMARINE")
-	in_padded := lib.Pkcs7Padding(in, 24)
+	in_padded := lib.Pkcs7Padding(in, 20)
 	fmt.Printf("IN:  %v\nOUT: %v\n", in, in_padded)
 }
 
 // Output:
 //
 // IN:  [89 69 76 76 79 87 32 83 85 66 77 65 82 73 78 69]
-// OUT: [89 69 76 76 79 87 32 83 85 66 77 65 82 73 78 69 8 8 8 8 8 8 8 8]
-
+// OUT: [89 69 76 76 79 87 32 83 85 66 77 65 82 73 78 69 4 4 4 4]
