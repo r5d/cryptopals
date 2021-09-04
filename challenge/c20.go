65df9556cea8e9c652023d84e174d40e6db8b56f
siddharth <s@ricketyspace.net>
2021-09-04 19:02:20 -0400

    challenge: go fmt

--- challenge/c20.go
+++ challenge/c20.go
@@ -153,16 +153,16 @@ func C20() {
 // Music's the clue, when I come your warned / Apocalyps
 // Haven't you ever heard of a MC-murderer? / This is th
 // Death wish, so come on, step to this / Hysterical ide
-// Friday the thirteenth, walking down Elm Street / You 
-// This is off limits, so your visions are blurry / All 
-// Terror in the styles, never error-files / Indeed I'm 
+// Friday the thirteenth, walking down Elm Street / You
+// This is off limits, so your visions are blurry / All
+// Terror in the styles, never error-files / Indeed I'm
 // For those that oppose to be level or next to this / I
 // Worse than a nightmare, you don't have to sleep a win
 // Flashbacks interfere, ya start to hear: / The R-A-K-I
-// Then the beat is hysterical / That makes Eric go get 
-// Soon the lyrical format is superior / Faces of death 
+// Then the beat is hysterical / That makes Eric go get
+// Soon the lyrical format is superior / Faces of death
 // MC's decaying, cuz they never stayed / The scene of a
-// The fiend of a rhyme on the mic that you know / It's 
+// The fiend of a rhyme on the mic that you know / It's
 // Melodies-unmakable, pattern-unescapable / A horn if w
 // I bless the child, the earth, the gods and bomb the r
 // Hazardous to your health so be friendly / A matter of
@@ -170,7 +170,7 @@ func C20() {
 // If not, my soul'll release! / The scene is recreated,
 // Cuz your about to see a disastrous sight / A performa
 // Lyrics of fury! A fearified freestyle! / The "R" is i
-// Make sure the system's loud when I mention / Phrases 
+// Make sure the system's loud when I mention / Phrases
 // You want to hear some sounds that not only pounds but
 // Then nonchalantly tell you what it mean to me / Stric
 // And I don't care if the whole crowd's a witness! / I'
@@ -183,25 +183,25 @@ func C20() {
 // Novocain ease the pain it might save him / If not, Er
 // Yo Rakim, what's up? / Yo, I'm doing the knowledge, E
 // Well, check this out, since Norby Walters is our agen
-// Kara Lewis is our agent, word up / Zakia and 4th and 
+// Kara Lewis is our agent, word up / Zakia and 4th and
 // Okay, so who we rollin' with then? We rollin' with Ru
 // Check this out, since we talking over / This def beat
 // I wanna hear some of them def rhymes, you know what I
 // Thinkin' of a master plan / 'Cuz ain't nuthin' but sw
 // So I dig into my pocket, all my money is spent / So I
-// So I start my mission, leave my residence / Thinkin' 
+// So I start my mission, leave my residence / Thinkin'
 // I need money, I used to be a stick-up kid / So I thin
 // I used to roll up, this is a hold up, ain't nuthin' f
 // But now I learned to earn 'cuz I'm righteous / I feel
 // Search for a nine to five, if I strive / Then maybe I
-// So I walk up the street whistlin' this / Feelin' out 
-// A pen and a paper, a stereo, a tape of / Me and Eric 
+// So I walk up the street whistlin' this / Feelin' out
+// A pen and a paper, a stereo, a tape of / Me and Eric
 // Fish, which is my favorite dish / But without no mone
-// 'Cuz I don't like to dream about gettin' paid / So I 
+// 'Cuz I don't like to dream about gettin' paid / So I
 // So now to test to see if I got pull / Hit the studio,
 // Rakim, check this out, yo / You go to your girl house
-// 'Cause my girl is definitely mad / 'Cause it took us 
+// 'Cause my girl is definitely mad / 'Cause it took us
 // Yo, I hear what you're saying / So let's just pump th
 // And count our money / Yo, well check this out, yo Eli
-// Turn down the bass down / And let the beat just keep 
+// Turn down the bass down / And let the beat just keep
 // And we outta here / Yo, what happened to peace? / Pea
